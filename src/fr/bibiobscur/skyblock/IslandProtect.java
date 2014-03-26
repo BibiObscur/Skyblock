4887c522ffd3a3901a1145fa965c12f6bace2ebe
utilisateur <utilisateur@Bastien-PC>
2014-03-26 18:39:50 +0100

    Ajout de protections sur les îles

--- src/fr/bibiobscur/skyblock/IslandProtect.java
+++ src/fr/bibiobscur/skyblock/IslandProtect.java
@@ -2,6 +2,7 @@ package fr.bibiobscur.skyblock;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -14,6 +15,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class IslandProtect implements Listener {
 
@@ -75,11 +77,58 @@ public class IslandProtect implements Listener {
 		    			e.getClickedBlock().getType() == Material.FENCE_GATE ||
 		    			e.getClickedBlock().getType() == Material.WOODEN_DOOR ||
 		    			e.getClickedBlock().getType() == Material.NOTE_BLOCK ||
-		    			e.getClickedBlock().getType() == Material.FURNACE)){
+		    			e.getClickedBlock().getType() == Material.FURNACE ||
+		    			e.getClickedBlock().getType() == Material.BURNING_FURNACE ||
+		    			e.getClickedBlock().getType() == Material.BREWING_STAND ||
+		    			e.getClickedBlock().getType() == Material.ANVIL ||
+		    			e.getClickedBlock().getType() == Material.TRAP_DOOR ||
+		    			e.getClickedBlock().getType() == Material.HOPPER ||
+				    	e.getClickedBlock().getType() == Material.REDSTONE_COMPARATOR ||
+						e.getClickedBlock().getType() == Material.REDSTONE_COMPARATOR_ON ||
+						e.getClickedBlock().getType() == Material.REDSTONE_COMPARATOR_OFF ||
+		    			e.getClickedBlock().getType() == Material.DROPPER ||
+		    			e.getClickedBlock().getType() == Material.DISPENSER ||
+		    			e.getClickedBlock().getType() == Material.JUKEBOX ||
+		    			e.getPlayer().getItemInHand().getType() == Material.LAVA_BUCKET ||
+	    				e.getPlayer().getItemInHand().getType() == Material.WATER_BUCKET ||
+	    				e.getPlayer().getItemInHand().getType() == Material.BUCKET)){
 					e.setCancelled(true);
 					e.getPlayer().sendMessage(ChatColor.RED + "Vous devez être sur votre île pour faire ceci !");
 				}
 	    	}
+	    	if(e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR)
+	    	{
+	    		if(!plugin.getDatas().isOnIsland(e.getPlayer()) &&
+	    				(e.getPlayer().getItemInHand().getType() == Material.LAVA_BUCKET ||
+	    				e.getPlayer().getItemInHand().getType() == Material.WATER_BUCKET ||
+	    				e.getPlayer().getItemInHand().getType() == Material.BUCKET ||
+	    				e.getPlayer().getItemInHand().getType() == Material.FLINT_AND_STEEL ||
+	    	    		e.getPlayer().getItemInHand().getType() == Material.MINECART ||
+	    				e.getPlayer().getItemInHand().getType() == Material.STORAGE_MINECART ||
+	    				e.getPlayer().getItemInHand().getType() == Material.POWERED_MINECART ||
+	    				e.getPlayer().getItemInHand().getType() == Material.HOPPER_MINECART)) {
+					e.setCancelled(true);
+					e.getPlayer().sendMessage(ChatColor.RED + "Vous devez être sur votre île pour faire ceci !");
+	    		}
+	    	}
+	    	
+    	}
+    }
+    
+    @EventHandler
+    public void interactEntityProtect(PlayerInteractEntityEvent e) {
+    	if(e.getPlayer().getWorld().getName().equals(plugin.getworldname())) {
+    		if(!e.getPlayer().isOp() && !plugin.getDatas().isOnIsland(e.getPlayer(), e.getRightClicked().getLocation())) {
+    			if(e.getRightClicked().getType() == EntityType.MINECART_CHEST || 
+    					e.getRightClicked().getType() == EntityType.MINECART_FURNACE ||
+    					e.getRightClicked().getType() == EntityType.MINECART_HOPPER ||
+    					e.getRightClicked().getType() == EntityType.ITEM_FRAME ||
+    					e.getRightClicked().getType() == EntityType.LEASH_HITCH ||
+    					e.getRightClicked().getType() == EntityType.HORSE) {
+    				e.setCancelled(true);
+    				e.getPlayer().sendMessage(ChatColor.RED + "Vous n'êtes pas sur votre île.");
+    			}
+    		}
     	}
     }
     
@@ -96,24 +145,17 @@ public class IslandProtect implements Listener {
 		    	
 		    	if(!plugin.getDatas().isOnIsland(player, e.getEntity().getLocation())) {
 
-				    	if(mob.equalsIgnoreCase("Chicken") ||
-				    			mob.equalsIgnoreCase("pig") ||
-				    			mob.equalsIgnoreCase("Cow") ||
-				    			mob.equalsIgnoreCase("Sheep") ||
-				    			mob.equalsIgnoreCase("Wolf") ||
-				    			mob.equalsIgnoreCase("Mushroom_Cow") ||
-				    			mob.equalsIgnoreCase("Ocelot") ||
-				    			mob.equalsIgnoreCase("Villager") ||
-				    			mob.equalsIgnoreCase("Horse")){
-				    		e.setDamage(0);
-				    	}
-				    	
-				    	/*if(e.getEntity() instanceof Player) {
-				    		Player player2 = (Player) e.getEntity();
-				    		player2.sendMessage(player.getName() + "a pris 20 dégats.");
-				    		//player.damage(20);
-				    	}
-				    	player.damage(10);*/
+				    if(mob.equalsIgnoreCase("Chicken") ||
+				    		mob.equalsIgnoreCase("pig") ||
+				    		mob.equalsIgnoreCase("Cow") ||
+				    		mob.equalsIgnoreCase("Sheep") ||
+				    		mob.equalsIgnoreCase("Wolf") ||
+				    		mob.equalsIgnoreCase("Mushroom_Cow") ||
+				    		mob.equalsIgnoreCase("Ocelot") ||
+				    		mob.equalsIgnoreCase("Villager") ||
+				    		mob.equalsIgnoreCase("Horse")){
+				    	e.setDamage(0);
+				    }
 			    	
 		    	}
 			}
